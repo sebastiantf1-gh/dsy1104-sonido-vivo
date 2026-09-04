2fb877b2ecba85c07cd45daf8f20a7dc9d05eb98
benjav892 <benjam.vasquezc@duocuc.cl>
2026-09-03 23:39:38 -0400

    completa validacion del login largo max de correo y contraseña junto con los correos validos-Benjamin Vasquez

--- js/funciones.js
+++ js/funciones.js
@@ -1,72 +1,67 @@
 const formulario = document.querySelector('#form-contacto');
 const patronCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-if(formulario){
+if (formulario) {
     formulario.addEventListener('submit', function (e) {
         e.preventDefault();
 
         const camposObligatorios = formulario.querySelectorAll('[required]');
         let formularioValido = true;
 
-        camposObligatorios.forEach(function (campo){
-            if (campo.value.trim() === ''){
+        camposObligatorios.forEach(function (campo) {
+            if (campo.value.trim() === '') {
                 campo.classList.add('campo-error');
                 formularioValido = false;
-            }else{
+            } else {
                 campo.classList.remove('campo-error');
             }
         });
 
         const correo = document.querySelector('#correo');
-        if(!patronCorreo.test(correo.value.trim())) {
+        if (!patronCorreo.test(correo.value.trim())) {
             correo.classList.add('campo-error');
-            formularioValido=false;
-        } 
-        
+            formularioValido = false;
+        }
+
         const mensajeConfirmado = document.querySelector('#mensaje-confirmado');
         mensajeConfirmado.textContent = formularioValido ? 'Consulta recibida.' : '';
     });
 }
 //Logica para el LOGIN.HTML 
 
-const formularioLogin = document.querySelector('#login'); 
+const formularioLogin = document.querySelector('#login');
 
-if(formularioLogin){
-    formularioLogin.addEventListener('submit', function(e){
+if (formularioLogin) {
+    formularioLogin.addEventListener('submit', function (e) {
         e.preventDefault();
 
         const correoLogin = document.querySelector('#correo-login');
-        const contrasena = document.querySelector('#contrasena'); 
-        let formularioLoginValido=true; 
-        const requeridos = document.querySelectorAll('[required]');
-        
-        
+        const contrasena = document.querySelector('#contrasena');
+        let formularioLoginValido = true;
+        const patronCorreoLogin = /^[^\s@]+@(duoc\.cl|profesor\.duoc\.cl|gmail\.com)$/;
 
-        if(!patronCorreo.test(correoLogin.value.trim())){
+        if (!patronCorreoLogin.test(correoLogin.value.trim()) || correoLogin.value.length > 100) {
             correoLogin.classList.add('campo-error');
-            document.querySelector('#error-correo').textContent="Ingrese un correo valido"
-            formularioLoginValido=false;
-        }else{
-            correoLogin.classList.remove('campo-error'); 
-            document.querySelector('#error-correo').textContent=''
+            document.querySelector('#error-correo').textContent = "Formato de correo no valido"
+            formularioLoginValido = false;
+        } else {
+            correoLogin.classList.remove('campo-error');
+            document.querySelector('#error-correo').textContent = ''
         }
 
-
-        if(contrasena.value.length<4 || contrasena.value.length>10){
-            contrasena.classList.add('campo-error'); 
+        if (contrasena.value.length < 4 || contrasena.value.length > 10) {
+            contrasena.classList.add('campo-error');
             document.querySelector('#error-contrasena').textContent = "La cantidad de caracteres es entre 4 y 10"
-            formularioLoginValido = false; 
-        }else{
+            formularioLoginValido = false;
+        } else {
             contrasena.classList.remove('campo-error')
-            document.querySelector('#error-contrasena').textContent=''
+            document.querySelector('#error-contrasena').textContent = ''
         }
 
-        
-
-        if(formularioLoginValido) {
+        if (formularioLoginValido) {
             document.querySelector('#mensaje-confirmacion').textContent = 'Inicia sesion correctamente'
         }
 
 
-    }); 
+    });
 }
